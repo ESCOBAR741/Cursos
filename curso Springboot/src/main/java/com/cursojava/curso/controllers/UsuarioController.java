5b4657b4972791a3474f1789cdb11184eb77ba01
Denilson Escobar <denilsonescobar741@gmail.com>
2022-04-16 13:06:04 -0400

    Curso Springboot

--- curso Springboot/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso Springboot/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.cursojava.curso.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value = "usuario/{id}")
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -28,7 +29,7 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios() {
         return usuarioDao.getUsuarios();
 
@@ -44,15 +45,13 @@ public class UsuarioController {
         return usuario;
 
     }
-    @RequestMapping(value = "usuario12")
-    public Usuario eliminar() {
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Denilson");
-        usuario.setApellido("Escobar");
-        usuario.setEmail("denilsonescobar741@gmail.com");
-        usuario.setTelefono("979841526");
-        return usuario;
+
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id) {
+        usuarioDao.eliminar(id);
+
     }
+
     @RequestMapping(value = "usuario123")
     public Usuario buscar() {
         Usuario  usuario = new Usuario();
--- curso Springboot/src/main/java/com/cursojava/curso/dao/UsuarioDaoimp.java
+++ curso Springboot/src/main/java/com/cursojava/curso/dao/UsuarioDaoimp.java
@@ -13,12 +13,20 @@ import java.util.List;
 public class UsuarioDaoimp implements UsuarioDao {
 
     @PersistenceContext
-    private EntityManager entityManager;
+    EntityManager entityManager;
 
+    @Transactional
     @Override
     public List<Usuario> getUsuarios() {
         String query = "FROM Usuario";
         return entityManager.createQuery(query).getResultList();
+    }
+
+
+    @Override
+    public void eliminar(Long id) {
+        Usuario usuario = entityManager.find(Usuario.class, id);
+        entityManager.remove(usuario);
     }
 
 }
