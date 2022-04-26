24bf6eb076718b89e1df586953b7a4daf44f83a9
Denilson Escobar <denilsonescobar741@gmail.com>
2022-04-26 16:25:10 -0400

    Numero Mayor y Menos
    Funciones.java

--- Curso-Java/Ejercicio_1.java
+++ Curso-Java/Ejercicio_1.java
@@ -15,36 +15,55 @@ import java.io.InputStreamReader;
 public class Ejercicio_1 {
     public static void main(String[] args) throws IOException{
 
+        System.out.println("Ingresa 1 numero ");
+        Integer numero1 = cargarNumero();
+        
+        System.out.println("Ingresa 2 numero ");
+        Integer numero2 = cargarNumero();
+
+        System.out.println("Ingresa 3 numero ");
+        Integer numero3 = cargarNumero();
+
+        calcularNumeroMayor(numero1, numero2, numero3);
+        calcularNumeroMenor(numero1, numero2, numero3);
+//calcularNumeroMayor(70, 30, 10); De esta manera paso valores a la fuerza o variables
+
+
+    }
+
+    private static Integer cargarNumero() throws IOException  {
         InputStreamReader capturarTeclado = new InputStreamReader(System.in);
         BufferedReader buffer = new BufferedReader(capturarTeclado);
 
-        System.out.println("Ingresa 1 numero ");
-        String strNumero1 = buffer.readLine();
-        Integer numero1 = Integer.parseInt(strNumero1);
-        
-        System.out.println("Ingresa 2 numero ");
-        String strNumero2 = buffer.readLine();
-        Integer numero2 = Integer.parseInt(strNumero2);
+        String strNumero = buffer.readLine();
+        Integer numero = Integer.parseInt(strNumero);
+        return numero;
+    }
 
-        System.out.println("Ingresa 3 numero ");
-        String strNumero3 = buffer.readLine();
-        Integer numero3 = Integer.parseInt(strNumero3);
- 
+    /**
+     * 
+     * @param numero1
+     * @param numero2
+     * @param numero3
+     */
+    private static void calcularNumeroMayor(Integer numero1, Integer numero2,Integer numero3) {
+
         Integer numeroMayor = numero1;
         if (numero2 >= numeroMayor) {
             numeroMayor = numero2;
         } 
-        
         if (numero3 > numeroMayor) {
             numeroMayor = numero3;
         }
         System.out.println("El numero mayor es " +numeroMayor);
+    }
+
+    private static void calcularNumeroMenor(Integer numero1, Integer numero2,Integer numero3) {
 
         Integer numeroMenor = numero1;
         if (numero2 < numeroMenor) {
             numeroMenor = numero2;
         }
-        
         if (numero3 < numeroMenor) {
             numeroMenor = numero3;
         }
