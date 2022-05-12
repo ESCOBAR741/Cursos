b9ab2d2f922c0c1ad8a09d914b03b36356bf8e75
Denilson Escobar <denilsonescobar741@gmail.com>
2022-05-12 12:51:49 -0400

    Funciones Euro - Dolar

--- Java-Testing-Course/1.- Simple Example JUnit/src/main/java/main.java
+++ Java-Testing-Course/1.- Simple Example JUnit/src/main/java/main.java
@@ -1,6 +1,6 @@
-public class main {
-    public static void main(String[] args ) {
+public class Main {
 
+    public static void main(String[] args) {
         double salary = 1000;
         System.out.println(MoneyUtil.format(salary));
     }
