e295049d13af08ba0acac307ce86922034e27f09
Denilson Escobar <denilsonescobar741@gmail.com>
2022-05-12 11:30:06 -0400

    Testing Java and Clean Code

---
+++ Java-Testing-Course/junitproject/src/main/java/MoneyUtil.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+public class MoneyUtil {
+    public static String format(double money) {
+        return format(money, "$");
+    }
+
+    public static String format(double money, String symbol) {
+        if(money <0) {
+            symbol = "- " + symbol;
+            money = money * (-1);
+        }
+        BigDecimal rounded = BigDecimal.valueOf(money).setScale(2, BigDecimal.ROUND_HALF_UP);
+        return symbol + rounded;    //-$ 1000
+    }
+}
---
+++ Java-Testing-Course/junitproject/src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args ) {
+
+        double salary = 1000;
+        System.out.println(MoneyUtil.format(salary));
+    }
+}
