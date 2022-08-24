b1229ced703ae08ba5c505238fafa54fe2ae99a0
Denilson Escobar <denilsonescobar741@gmail.com>
2022-08-24 16:22:23 -0400

    reloj
    constructor

---
+++ ust/clasereloj/src/RadioReloj.java
@@ -0,0 +1,2 @@
+public class RadioReloj {
+}
---
+++ ust/clasereloj/src/Reloj.java
@@ -0,0 +1,23 @@
+public class Reloj {
+
+    public static void mutadores(String[] args) {
+
+    }
+
+    public static void accesadores(String[] args) {
+
+    }
+
+    public static void formatohoraminuto(String[] args) {
+
+    }
+
+    public static void minuto(String[] args) {
+
+    }
+
+    public static void imprimir(String[] args) {
+
+    }
+
+}
