f76be2ca4894e17f7b70a9a74f3f668ece62a957
Denilson Escobar <denilsonescobar741@gmail.com>
2022-11-07 14:47:38 -0300

    login swing

--- ust/clasereloj/src/RadioReloj.java
+++
@@ -1,5 +0,0 @@
-public class RadioReloj {
-    public void imprimir() {
-
-    }
-}
--- ust/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Facturas fact = new Facturas(10,20,30);
-        fact.imprimir();
-    }
-}
