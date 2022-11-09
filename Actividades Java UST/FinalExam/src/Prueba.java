f32f6b3b0f95f8ce151add50a227ca53275d5294
Denilson Escobar <denilsonescobar741@gmail.com>
2022-11-09 16:54:09 -0300

    api
    conexion db

---
+++ Actividades Java UST/FinalExam/src/Prueba.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class Prueba {
+    private static final String driver  = "com.mysql.cj.jdbc.Driver";
+    private static final String db      = "jdbc:mysql://localhost:3306/usuarios"; //Nuestra tabla
+    private static final String usuario = "root";
+    private static final String clave   = "";
+
+
+    public static Connection conexion() {
+        Connection conex = null;
+        try {
+            Class.forName(driver);
+            conex = DriverManager.getConnection(db, usuario, clave);
+        } catch (Exception e) {
+            System.out.println("Error 4062" +e.getMessage().toString());
+        }
+        return conex;
+    }
+
+    public static void main(String[] args) {
+        Prueba run = new Prueba();
+        Connection conect = Prueba.conexion();
+        if (conect != null) {
+            System.out.println("Funciona");
+        }
+    }
+}
