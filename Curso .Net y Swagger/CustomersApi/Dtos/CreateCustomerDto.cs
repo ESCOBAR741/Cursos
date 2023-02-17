3f5b1539cc0e09f3c090d5a7192e341020475b43
Denilson Escobar <denilsonescobar741@gmail.com>
2023-02-16 21:49:18 -0300

    Update Customer Dto

---
+++ Curso .Net y Swagger/CustomersApi/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomersApi.Dtos
+{
+    public class CreateCustomerDto
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Adrres { get; set; }
+    }
+}
---
+++ Curso .Net y Swagger/CustomersApi/Dtos/Customers.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomersApi.Dtos
+{
+    public class CustomerDto
+    {
+
+        public long Id { get; set; }
+
+        public string FirstNAme { get; set; }
+        public string LastNAme { get; set;}
+        public string Email { get; set;}
+        public string Phone { get; set;}
+        public string Adrres { get; set;}
+
+    }
+}
