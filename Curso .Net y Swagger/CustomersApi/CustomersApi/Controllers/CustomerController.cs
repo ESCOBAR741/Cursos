21782fb8ba2e42876cff3c52cc3bd5c9c3c1f618
Denilson Escobar <denilsonescobar741@gmail.com>
2023-02-22 22:00:34 -0300

    Protocolo http Api

---
+++ Curso .Net y Swagger/CustomersApi/CustomersApi/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CustomersApi.Dtos;
+
+namespace CustomersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public async Task<List<CustomerDto>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<CustomerDto> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+
+         }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<CustomerDto> CreateCustomer(CreateCustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDto> UpdateCustomerDto(CustomerDto customer)
+        { 
+            throw new NotImplementedException();
+        }
+
+    }
+}
