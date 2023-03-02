86a36ee660a85f58a4a5cebac426ef09e211fb9a
Denilson Escobar <denilsonescobar741@gmail.com>
2023-03-01 23:17:27 -0300

    DB Conect

---
+++ Curso .Net y Swagger/CustomersApi/CustomersApi/Repositories/CustomerDataBaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomersApi.Repositories
+{
+    public class CustomerDataBaseContext : DbContext
+    {
+        public DbSet<CustomerEntity> Customer { get; set; }
+
+        public async Task<CustomerEntity> Get(long id)
+        {
+            return await Customer.FirstAsync(x => x.Id == id);
+        }
+        public async Task<CustomerEntity> Add(CustomerEntity entity)
+        {
+
+        }
+    }
+
+    public class CustomerEntity
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
